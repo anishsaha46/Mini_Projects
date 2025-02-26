1f92ff51572eb6422520f0552654e60497715f91
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-26 23:28:20 +0530

    initialize server with database connection and HTTP setup

---
+++ Course_Selling/server.js
@@ -0,0 +1,18 @@
+import http from "http";
+import dotenv from "dotenv";
+import app from "./src/app.js";
+import connectDB from "../src/config/db.js";
+
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+// create an http server
+const server = http.createServer(app);
+
+// start the server
+const PORT = process.env.PORT || 5000;
+Server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
